6d05e6a412362eb691e3ff9d12272ff02f25517c
sh1r0oo <sarashad2910@gmail.com>
2025-05-02 22:27:26 +0600

    Added Reference Page (Redone)

--- next.config.js
+++ next.config.js
@@ -4,81 +4,11 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { 
-    unoptimized: true,
-    formats: ['image/avif', 'image/webp'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-  },
-  webpack: (config, { isServer, dev }) => {
-    // Advanced optimization for production
-    if (!dev) {
-      // Tree shaking and code splitting
-      config.optimization.minimize = true;
-      config.optimization.splitChunks = {
-        chunks: 'all',
-        maxInitialRequests: 5,
-        maxAsyncRequests: 5,
-        minSize: 20000,
-        cacheGroups: {
-          framework: {
-            test: /[\/]node_modules[\/](react|react-dom|next)[\/]/,
-            name: 'framework',
-            priority: 40,
-            enforce: true,
-          },
-          vendors: {
-            test: /[\/]node_modules[\/]/,
-            priority: 30,
-            name: 'vendors',
-          },
-          commons: {
-            name: 'commons',
-            minChunks: 2,
-            priority: 20,
-          },
-        },
-      };
-
-      // Aggressive code optimization
-      config.optimization.minimizer.push(
-        new (require('terser-webpack-plugin'))({
-          terserOptions: {
-            compress: {
-              drop_console: true,
-              dead_code: true,
-              passes: 2,
-            },
-            mangle: true,
-          },
-        })
-      );
-    }
-
-    // Efficient caching
-    config.cache = {
-      type: 'filesystem',
-      maxMemoryGenerations: 3,
-      buildDependencies: {
-        config: [__filename],
-      },
-    };
-
-    // Performance hints
-    config.performance = {
-      hints: dev ? false : 'warning',
-      maxEntrypointSize: 512000,
-      maxAssetSize: 512000,
-    };
-
+  images: { unoptimized: true },
+  webpack: (config, { isServer, webpack }) => {
+    // Disable webpack cache to resolve caching errors
+    config.cache = false;
     return config;
-  },
-  compress: true,
-  poweredByHeader: false,
-  productionBrowserSourceMaps: false,
-  experimental: {
-    optimizePackageImports: ['@/components', 'recharts', 'framer-motion'],
-    serverActions: true,
-    optimisticClientCache: true,
   },
 };
 
